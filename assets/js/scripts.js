e374f5a60aa496bf0b95e9ca440ac2e29f6309ae
LuucasNunes <llucas-nunes@hotmail.com>
2026-03-27 23:38:15 -0300

    fix/validadores-de-erros-NaN

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $(".valores").maskMoney({ prefix: 'R$ ', allowNegative: false, thousands: '.', decimal: ',', affixesStay: false });
 });
 
-//Fnção agora evita NaN 
+//Função agora evita NaN 
 function getValorNumerico(campo) {
     let valor = $("#" + campo).val();
 
@@ -38,7 +38,13 @@ function adicionarMeses(dataBase, meses) {
     return novaData;
 }
 
+function limparTabela(tabela) {
+    $("#corpo_" + tabela).html("");
+}
+
 function calcula_price() {
+
+    limparTabela("tabela_price");
 
     let select = document.querySelector('#periodo_juros');
     let periodo_juros = select.value;
@@ -49,26 +55,42 @@ function calcula_price() {
     let taxa_juros = getValorNumerico("taxa_juros_price") / 100;
     let n_parcelas = getValorNumerico("num_parcelas_price");
 
+    //validadores
+    if (!valor_financiamento || !n_parcelas) {
+        alert("Preencha os campos corretamente!");
+        return;
+    }
+
+    if (valor_entrada > valor_financiamento) {
+        alert("A entrada não pode ser maior que o financiamento");
+        return;
+    }
     if (periodo_juros == "trimestral") {
         taxa_juros = (1 + taxa_juros) ** 3 - 1;
-        n_parcelas = n_parcelas * 3;
     } else if (periodo_juros == "semestral") {
         taxa_juros = (1 + taxa_juros) ** 6 - 1;
-        n_parcelas = n_parcelas * 6;
     }
 
     let valor_presente = valor_financiamento - valor_entrada;
 
-    let valor_parcela =
-        valor_presente *
-        (
-            (taxa_juros * ((1 + taxa_juros) ** n_parcelas)) /
-            (((1 + taxa_juros) ** n_parcelas) - 1)
-        );
-
+    //valida se a taxa de juros é >= 0 
+    if (taxa_juros === 0) {
+        valor_parcela = valor_presente / n_parcelas;
+    } else {
+        valor_parcela =
+            valor_presente *
+            (
+                (taxa_juros * ((1 + taxa_juros) ** n_parcelas)) /
+                (((1 + taxa_juros) ** n_parcelas) - 1)
+            );
+    }
     let parcelas = [];
     let saldo_devedor = valor_presente;
     let data_base = new Date();
+
+    let incremento = 1;
+    if (periodo_juros == "trimestral") incremento = 3;
+    else if (periodo_juros == "semestral") incremento = 6;
 
     for (let i = 1; i <= n_parcelas; i++) {
 
@@ -76,7 +98,7 @@ function calcula_price() {
         let amortizacao = valor_parcela - juros;
 
         saldo_devedor = saldo_devedor - amortizacao;
-        let data_parcela = adicionarMeses(data_base, i);
+        let data_parcela = adicionarMeses(data_base, i * incremento);
 
         parcelas.push({
             parcela: i,
@@ -84,7 +106,7 @@ function calcula_price() {
             valor_parcela: valor_parcela,
             amortizacao: amortizacao,
             juros: juros,
-            saldo_devedor: saldo_devedor
+            saldo_devedor: saldo_devedor < 0 ? 0 : saldo_devedor
         });
     }
 
